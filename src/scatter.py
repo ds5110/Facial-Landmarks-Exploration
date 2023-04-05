b41e03dd76f7f9fe1b1a5b3a73bc15b331a5171f
yinna <yinnalucky@gmail.com>
2023-04-05 15:54:50 -0400

    modify the scatter

--- src/scatter.py
+++ src/scatter.py
@@ -11,48 +11,26 @@ adult = landmarks[landmarks['baby'] == 0]
 def scatter(df, category):
     # calculate the means, medians,stds,range of x,y
     x_means = []
-    x_medians = []
-    x_stds = []
-    x_ranges = []
     y_means = []
-    y_medians = []
-    y_stds = []
-    y_ranges = []
 
     for i in range(68):
-        x_col = 'x{}'.format(i)
-        y_col = 'y{}'.format(i)
-        x_data = df[x_col]
-        y_data = df[y_col]
-        x_mean = np.mean(x_data)
-        x_median = np.median(x_data)
-        x_std = np.std(x_data)
-        x_range = np.max(x_data) - np.min(x_data)
-        y_mean = np.mean(y_data)
-        y_median = np.median(y_data)
-        y_std = np.std(y_data)
-        y_range = np.max(y_data) - np.min(y_data)
+        x_col = 'norm_cenrot-x{}'.format(i)
+        y_col = 'norm_cenrot-y{}'.format(i)
+        x_mean = np.mean(df[x_col])
         x_means.append(x_mean)
-        x_medians.append(x_median)
-        x_stds.append(x_std)
-        x_ranges.append(x_range)
+        x_median = np.median(df[x_col])
+        x_std = np.std(df[x_col])
+        x_range = np.max(df[x_col]) - np.min(df[x_col])
+        y_mean = np.mean(df[y_col])
         y_means.append(y_mean)
-        y_medians.append(y_median)
-        y_stds.append(y_std)
-        y_ranges.append(y_range)
+        y_median = np.median(df[y_col])
+        y_std = np.std(df[y_col])
+        y_range = np.max(df[y_col]) - np.min(df[y_col])
+        print('Column x{}: Mean={:.2f}, Median={:.2f}, Std={:.2f}, Range={:.2f}'.format(i, x_mean, x_median,
+                                                                                        x_std, x_range))
+        print('Column y{}: Mean={:.2f}, Median={:.2f}, Std={:.2f}, Range={:.2f}'.format(i, y_mean, y_median,
+                                                                                        y_std, y_range))
 
-    # output the outcome
-    for i in range(68):
-        print('Column x{}: Mean={:.2f}, Median={:.2f}, Std={:.2f}, Range={:.2f}'.format(i, x_means[i], x_medians[i],
-                                                                                        x_stds[i], x_ranges[i]))
-        print('Column y{}: Mean={:.2f}, Median={:.2f}, Std={:.2f}, Range={:.2f}'.format(i, y_means[i], y_medians[i],
-                                                                                        y_stds[i], y_ranges[i]))
-
-    '''
-    for i, (x, y) in enumerate(zip(x_mean, y_mean)):
-        plt.scatter(x, y)
-        plt.text(x + 0.02, y + 0.02, str(i), fontsize=10)
-    '''
 
     plt.scatter(x_means, y_means)
 
@@ -108,5 +86,3 @@ def scatter(df, category):
 scatter(infant, "infant")
 
 scatter(adult, "adult")
-
-
