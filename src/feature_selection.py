b01f2e68c58e1aa872a6eb44b550de8e74b555c5
LiyangSong <song.liy@northeastern.edu>
2023-04-08 19:27:32 -0400

    feature selection implementation

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,95 +1,17 @@
 import pandas as pd
-from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import SelectKBest, VarianceThreshold, f_classif, mutual_info_classif, SequentialFeatureSelector, RFE, SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_validate
-    
+from sklearn.model_selection import cross_validate, cross_val_predict
+from sklearn.metrics import classification_report, confusion_matrix
 
-def feature_selector_pipelines():
-    n_features = [5, 10, 20, 30, 40, 50]
-    pipelines = {}
-    feature_selectors = {}
 
-    for enum, i in enumerate(n_features):
-        # SelectKBest using f_classif
-        pipeline_name = "pipeline_{}".format(enum + 1)
-        selector_name = "f_classif_param_{}".format(i)
-        selector_method = SelectKBest(f_classif, k=i)
-        pipeline = Pipeline([
-            (selector_name, selector_method),
-            ("logistic_regression", LogisticRegression())
-        ])
-        pipelines[pipeline_name] = pipeline
-        feature_selectors[pipeline_name] = (selector_name, selector_method)
-
-        # SelectKBest using mutual_info_classif
-        pipeline_name = "pipeline_{}".format(enum + 7)
-        selector_name = "mutual_info_classif_param_{}".format(i)
-        selector_method = SelectKBest(mutual_info_classif, k=i)
-        pipeline = Pipeline([
-            (selector_name, selector_method),
-            ("logistic_regression", LogisticRegression())
-        ])
-        pipelines[pipeline_name] = pipeline
-        feature_selectors[pipeline_name] = (selector_name, selector_method)
-        
-        # Forward Feature Selector
-        pipeline_name = "pipeline_{}".format(enum + 13)
-        selector_name = "sequential_feature_selector_param_{}".format(i)
-        selector_method = SequentialFeatureSelector(estimator=LogisticRegression(), n_features_to_select=i, direction="forward", n_jobs=-1)
-        pipeline = Pipeline([
-            (selector_name, selector_method),
-            ("logistic_regression", LogisticRegression())
-        ])
-        pipelines[pipeline_name] = pipeline
-        feature_selectors[pipeline_name] = (selector_name, selector_method)
-
-        # RFE
-        pipeline_name = "pipeline_{}".format(enum + 19)
-        selector_name = "rfe_param_{}".format(i)
-        selector_method = RFE(estimator=LogisticRegression(), n_features_to_select=i)
-        pipeline = Pipeline([
-            (selector_name, selector_method),
-            ("logistic_regression", LogisticRegression())
-        ])
-        pipelines[pipeline_name] = pipeline
-        feature_selectors[pipeline_name] = (selector_name, selector_method)
-
-        # LASSO Regularization (L1)
-        pipeline_name = "pipeline_{}".format(enum + 25)
-        selector_name = "lasso_regulation_param_{}".format(i)
-        logistic = LogisticRegression(penalty="l1", solver="liblinear", random_state=7)
-        selector_method = SelectFromModel(estimator=logistic, max_features=i)
-        pipeline = Pipeline([
-            (selector_name, selector_method),
-            ("logistic_regression", LogisticRegression())
-        ])
-        pipelines[pipeline_name] = pipeline
-        feature_selectors[pipeline_name] = (selector_name, selector_method)
-
-        # Random Forest Classifier
-        pipeline_name = "pipeline_{}".format(enum + 31)
-        selector_name = "random_forest_classifier_param_{}".format(i)
-        random_forest = RandomForestClassifier(random_state=7)
-        selector_method = SelectFromModel(estimator=random_forest, max_features=i)
-        pipeline = Pipeline([
-            (selector_name, selector_method),
-            ("logistic_regression", LogisticRegression())
-        ])
-        pipelines[pipeline_name] = pipeline
-        feature_selectors[pipeline_name] = (selector_name, selector_method)
-    
-    return pipelines, feature_selectors
-        
-
-def feature_seletion(pipelines, feature_selectors, df, feature_cols):
-    df_X = df.loc[:, X_cols]
+# Remove features with low variance    
+def variance_threshold(df, feature_cols):
+    df_X = df.loc[:, feature_cols]
     X = df_X.values
     y = df.loc[:, "baby"].values
-    print("Shape of feature matrix: {}.".format(X.shape))
 
-    # Remove features with low variance
     method = VarianceThreshold()
     method.fit(X, y)
     mask = method.get_support()
@@ -97,33 +19,123 @@ def feature_seletion(pipelines, feature_selectors, df, feature_cols):
     X_cols_new = df_X.columns[mask]
     df_X_new = df.loc[:, X_cols_new]
     X_new = df_X_new.values
-    print("Shape of feature matrix after remove low variance features: {}.".format(X_new.shape))
 
-    for i in range(1, 37):
-        pipeline_name = "pipeline_{}".format(i)
-        pipeline = pipelines[pipeline_name]
-        selector_name, selector_method = feature_selectors[pipeline_name]
+    return df_X_new, X_new, y
 
-        print("Feature Selector: ", selector_name)
 
+# Six different feature selection methods as well as all features
+def feature_selection(df, feature_cols):
+    methods = ["all_features",
+               "f_classif", 
+               "mutual_info_classif", 
+               "sequential_feature_selector", 
+               "rfe", 
+               "lasso_regulation", 
+               "random_forest_classifier"]
+    n_features = [2, 3, 5, 10, 15, 20, 30, 50]
+    feature_names = {}
+    df_X_new, X_new, y = variance_threshold(df, feature_cols)
+
+    # All features
+    selector_name = methods[0]
+    feature_names[selector_name] = df_X_new.columns
+
+    for i in n_features:
+        # SelectKBest using f_classif
+        selector_name = "{}_num_{}".format(methods[1], i)
+        selector_method = SelectKBest(f_classif, k=i)
         selector_method.fit(X_new, y)
         feature_mask = selector_method.get_support()
-        feature_names = df_X_new.columns[feature_mask]
-        print("Selected Features: ", list(feature_names))
+        feature_names[selector_name] = df_X_new.columns[feature_mask]
 
-        pipeline.fit(X_new, y)
-        scores = cross_validate(pipeline, X_new, y, cv=5, return_train_score=True)
-        print("Cross Validation Train Score: {:.2e} +/- {:.2e}.".format(scores["train_score"].mean(), scores["train_score"].std()))
-        print("Cross Validation Test Score: {:.2e} +/- {:.2e}.".format(scores["test_score"].mean(), scores["test_score"].std()))
+        # SelectKBest using mutual_info_classif
+        selector_name = "{}_num_{}".format(methods[2], i)
+        selector_method = SelectKBest(mutual_info_classif, k=i)
+        selector_method.fit(X_new, y)
+        feature_mask = selector_method.get_support()
+        feature_names[selector_name] = df_X_new.columns[feature_mask]
+        
+        # Forward Feature Selector
+        selector_name = "{}_num_{}".format(methods[3], i)
+        selector_method = SequentialFeatureSelector(estimator=LogisticRegression(), n_features_to_select=i, direction="forward", n_jobs=-1)
+        selector_method.fit(X_new, y)
+        feature_mask = selector_method.get_support()
+        feature_names[selector_name] = df_X_new.columns[feature_mask]
+
+        # RFE
+        selector_name = "{}_num_{}".format(methods[4], i)
+        selector_method = RFE(estimator=LogisticRegression(), n_features_to_select=i)
+        selector_method.fit(X_new, y)
+        feature_mask = selector_method.get_support()
+        feature_names[selector_name] = df_X_new.columns[feature_mask]
+
+        # LASSO Regularization (L1)
+        selector_name = "{}_num_{}".format(methods[5], i)
+        logistic = LogisticRegression(penalty="l1", solver="liblinear", random_state=7)
+        selector_method = SelectFromModel(estimator=logistic, max_features=i)
+        selector_method.fit(X_new, y)
+        feature_mask = selector_method.get_support()
+        feature_names[selector_name] = df_X_new.columns[feature_mask]
+
+        # Random Forest Classifier
+        selector_name = "{}_num_{}".format(methods[6], i)
+        random_forest = RandomForestClassifier(random_state=7, n_jobs=-1)
+        selector_method = SelectFromModel(estimator=random_forest, max_features=i)
+        selector_method.fit(X_new, y)
+        feature_mask = selector_method.get_support()
+        feature_names[selector_name] = df_X_new.columns[feature_mask]
+    
+    return feature_names
+        
+
+# Test selected features using logistic regression
+def feature_seletion_performance(feature_names, df, feature_cols):
+    df_X_new, X_new, y = variance_threshold(df, feature_cols)
+    column_names = ["selector_name",
+                    "CV_train_score_mean",
+                    "CV_train_score_std",
+                    "CV_test_score_mean",
+                    "CV_test_score_mean",
+                    "CM_true_adult",
+                    "CM_false_infant",
+                    "CM_false_adult",
+                    "CM_true_infant"] + feature_cols
+    feature_selection_result = pd.DataFrame(index=range(49), columns=column_names)
+
+    for i in range(49):
+        selector_name = list(feature_names.keys())[i]
+        selected_features = list(feature_names.values())[i]
+        df_X_selected = df_X_new.loc[:, selected_features]
+        X_selected = df_X_selected.values
+
+        feature_selection_result.iloc[i, 0] = selector_name
+        logistic = LogisticRegression(random_state=7)
+        logistic.fit(X_selected, y)
+        scores = cross_validate(logistic, X_selected, y, cv=5, return_train_score=True)
+        
+        feature_selection_result.iloc[i, 1] = scores["train_score"].mean()
+        feature_selection_result.iloc[i, 2] = scores["train_score"].std()
+        feature_selection_result.iloc[i, 3] = scores["test_score"].mean()
+        feature_selection_result.iloc[i, 4] = scores["test_score"].std()
+
+        y_pred = cross_val_predict(logistic, X_selected, y, cv=5)
+        cm = confusion_matrix(y, y_pred, labels=[0, 1])
+        feature_selection_result.iloc[i, 5:9] = cm.flatten().tolist()
+
+        feature_selection_result.iloc[i, 9:] = [feature_col in selected_features for feature_col in feature_cols]
+
+    return feature_selection_result
 
 
 def main():
     df = pd.read_csv("outcome/prev/merged_landmarks.csv")
-    feature_cols = [col for col in df.columns if 'norm_cenrot-' in col]
-    pipelines, feature_selectors = feature_selector_pipelines()
-    feature_seletion(pipelines, feature_selectors, df, feature_cols)
+    feature_cols = [col for col in df.columns if "norm_cenrot-" in col]
+
+    feature_names = feature_selection(df, feature_cols)
+    feature_selection_result = feature_seletion_performance(feature_names, df, feature_cols)
+    feature_selection_result.to_csv('outcome/feature_selection/feature_selection.csv', index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
--- src/feature_selection_plots.py
+++ src/feature_selection_plots.py
@@ -1,45 +1,77 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
-df = pd.read_csv("outcome/prev/merged_landmarks.csv")
-x_cols = [col for col in df.columns if 'norm_cenrot-x' in col]
-y_cols = [col for col in df.columns if 'norm_cenrot-y' in col]
+def variance_matrix():
+    placeholder
 
-x_mean = df[x_cols].mean(axis=0)
-y_mean = df[y_cols].mean(axis=0)
 
-fig, ax = plt.subplots()
-x_abs_max = max(abs(x_mean))
-y_abs_max = max(abs(y_mean))
-ax.set_xlim([-x_abs_max, x_abs_max])
-ax.set_ylim([-y_abs_max, y_abs_max])
-ax.scatter(x_mean, y_mean, alpha=0.5)
+def confusion_matrix():
+    placeholder
 
-ax.axhline(0, color='black', linewidth=0.5)
-ax.axvline(0, color='black', linewidth=0.5)
 
-selected_features = ['norm_cenrot-y6', 'norm_cenrot-y7', 'norm_cenrot-x8', 'norm_cenrot-y9', 'norm_cenrot-x21', 'norm_cenrot-x22', 'norm_cenrot-x23', 'norm_cenrot-x25', 'norm_cenrot-y28', 'norm_cenrot-x30', 'norm_cenrot-y30', 'norm_cenrot-x34', 'norm_cenrot-x38', 'norm_cenrot-y40', 'norm_cenrot-x42', 'norm_cenrot-x47', 'norm_cenrot-y48', 'norm_cenrot-y53', 'norm_cenrot-x55', 'norm_cenrot-y60']
-for feature in selected_features:
-    landmark_label = feature.split("-")[1]
-    if feature in x_cols:
-        landmark_x = x_mean[feature]
-        landmark_y = y_mean[x_cols.index(feature)]
-        ax.scatter(landmark_x, landmark_y, c="green")
-        ax.annotate(landmark_label, (landmark_x, landmark_y), textcoords="offset points", xytext=(-5,-5))
-        if landmark_x > 0:
-            ax.axhline(y=landmark_y, xmin=0.5, xmax=0.5 + landmark_x / ax.get_xlim()[1] / 2, color='green', linewidth=0.5)
-        if landmark_x < 0:
-            ax.axhline(y=landmark_y, xmax=0.5, xmin=0.5 + landmark_x / ax.get_xlim()[1] / 2, color='green', linewidth=0.5)
+def landmark_plots(df, x_cols, y_cols, feature_selection_result):
+    x_mean = df[x_cols].mean(axis=0)
+    y_mean = df[y_cols].mean(axis=0)
+    x_abs_max = max(abs(x_mean))
+    y_abs_max = max(abs(y_mean))
+    
+    methods = ["f_classif", 
+               "mutual_info_classif", 
+               "sequential_feature_selector", 
+               "rfe", 
+               "lasso_regulation", 
+               "random_forest_classifier"]
+    for method in methods:
+        method_df = feature_selection_result.loc[feature_selection_result["selector_name"].str.contains(method)]
+        
+        fig, axs = plt.subplots(2, 4, figsize=(20, 10))
+        for i, ax in enumerate(axs.flat):
+            ax.set_xlim([x_abs_max, -x_abs_max])
+            ax.set_ylim([y_abs_max, -y_abs_max])
+            ax.scatter(x_mean, y_mean, alpha=0.5)
 
-    if feature in y_cols:
-        landmark_x = x_mean[y_cols.index(feature)]
-        landmark_y = y_mean[feature]
-        ax.scatter(landmark_x, landmark_y, c="green")
-        ax.annotate(landmark_label, (landmark_x, landmark_y), textcoords="offset points", xytext=(5,5))
-        if landmark_y > 0:
-            ax.axvline(x=landmark_x, ymin=0.5, ymax=(0.5 + landmark_y / ax.get_ylim()[1] / 2), color='green', linewidth=0.5)
-        if landmark_y < 0:
-            ax.axvline(x=landmark_x, ymax=0.5, ymin=(0.5 + landmark_y / ax.get_ylim()[1] / 2), color='green', linewidth=0.5)
+            ax.axhline(0, color='black', linewidth=0.5)
+            ax.axvline(0, color='black', linewidth=0.5)
+            
+            feature_names = method_df.columns[method_df.iloc[i, :] == True]
+            for feature_name in feature_names:
+                landmark_label = feature_name.split("-")[1]
+                if feature_name in x_cols:
+                    landmark_x = x_mean[feature_name]
+                    landmark_y = y_mean[x_cols.index(feature_name)]
+                    ax.scatter(landmark_x, landmark_y, c="green")
+                    ax.annotate(landmark_label, (landmark_x, landmark_y), textcoords="offset points", xytext=(-20, -10))
+                    if landmark_x > 0:
+                        ax.axhline(y=landmark_y, xmin=0.5, xmax=0.5 + landmark_x / ax.get_xlim()[1] / 2, color='green')
+                    else:
+                        ax.axhline(y=landmark_y, xmax=0.5, xmin=0.5 + landmark_x / ax.get_xlim()[1] / 2, color='green')
 
-plt.show()
+                if feature_name in y_cols:
+                    landmark_x = x_mean[y_cols.index(feature_name)]
+                    landmark_y = y_mean[feature_name]
+                    ax.scatter(landmark_x, landmark_y, c="green")
+                    ax.annotate(landmark_label, (landmark_x, landmark_y), textcoords="offset points", xytext=(5, 5))
+                    if landmark_y > 0:
+                        ax.axvline(x=landmark_x, ymin=0.5, ymax=(0.5 + landmark_y / ax.get_ylim()[1] / 2), color='green')
+                    else:
+                        ax.axvline(x=landmark_x, ymax=0.5, ymin=(0.5 + landmark_y / ax.get_ylim()[1] / 2), color='green')
+            ax.set_title("{}\n CV Test Score = {:.3f} +/- {:.3f}".format(method_df.iloc[i, 0], method_df.iloc[i, 3], method_df.iloc[i, 4]), pad=20)
+        fig.subplots_adjust(hspace=0.4)
+        fig.savefig("outcome/feature_selection/features_{}.png".format(method), dpi=300)
+
+
+def main():
+    feature_selection_result = pd.read_csv("outcome/feature_selection/feature_selection.csv")
+    df = pd.read_csv("outcome/prev/merged_landmarks.csv")
+    
+    feature_cols = [col for col in df.columns if 'norm_cenrot-' in col]
+    x_cols = [col for col in df.columns if 'norm_cenrot-x' in col]
+    y_cols = [col for col in df.columns if 'norm_cenrot-y' in col]
+
+    landmark_plots(df, x_cols, y_cols, feature_selection_result)
+
+
+if __name__ == "__main__":
+    main()
